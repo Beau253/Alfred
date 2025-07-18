9aef19b7b3e5f32945348cdcce6ad8dc0af08179
Beau253 <103298340+Beau253@users.noreply.github.com>
2025-07-18 23:34:06 +1000

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ logger.setLevel(logging.INFO)
 
 file_handler = logging.FileHandler("alfred.log")
 file_handler.setFormatter(log_formatter)
+logger.addHandler(file_handler)
 
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(log_formatter)
